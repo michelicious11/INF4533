f8c844ceaea7e3abe2927176ede3bc35c4db140e
Simon-Pierre Fortin <fors31@uqo.ca>
2019-04-20 16:36:15 -0400

    Bugfix du serveur

--- serveur.js
+++ serveur.js
@@ -44,23 +44,33 @@ app.listen(PORT, function() {
 
 var socket = new net.Socket();
 
-// Le format de la connexion est socket.connect({port}, {adresse}, {function})
-// Veuillez modifier le port et l'adresse pour vous connecter au serveur voulu
-socket.connect(3034, "localhost", function () { 
-    console.log("Connecté au serveur distant");
-    socket.on("data", function(data){
-        data = JSON.parse(data);
-        // Comparaison des courriels sur JSON reçu avec ceux du JSON du serveur local
-        for (var i = 0; i < Object.values(data).length; i++) {
-            for (var k = 0; k < Object.values(data)[i]["emails"].length; k++){
-                if (Object.values(my_obj)[i]["emails"].indexOf(Object.values(data)[i]["emails"][k]) < 0){
-                    Object.values(my_obj)[i]["emails"].unshift(Object.values(data)[i]["emails"][k]); 
+// Test de connexion à un serveur distant
+connect_to_distant(3034, "localhost");
+
+
+// Fonction permettant de créer des connexions à des serveurs distants
+// La mise à jour des courriels s'effectuent pas une comparaison entre serveurs à la connexion
+function connect_to_distant (port_distant, adresse_distant) {
+    console.log("Tentative de connexion distante au " + adresse_distant + " sur le port " + port_distant);
+    socket.connect(port_distant, adresse_distant, function () { 
+        console.log("Connecté au serveur distant");
+        var chunks = [];
+        socket.on("data", function(data){
+            chunks.push(data);
+            var new_data = JSON.parse(chunks);
+            // Comparaison des courriels sur JSON reçu avec ceux du JSON du serveur local
+            for (var i = 0; i < Object.values(new_data).length; i++) {
+                for (var k = 0; k < Object.values(new_data)[i]["emails"].length; k++){
+                    if (Object.values(my_obj)[i]["emails"].indexOf(Object.values(new_data)[i]["emails"][k]) < 0){
+                        Object.values(my_obj)[i]["emails"].unshift(Object.values(new_data)[i]["emails"][k]); 
+                    }
                 }
             }
-        }
+
+        });
+        socket.write(JSON.stringify(my_obj));
     });
-    socket.write(JSON.stringify(my_obj));
-});
+};
 
 socket.on("error", function(e){
     if (e.code === "ECONNREFUSED"){
@@ -74,17 +84,19 @@ var server = net.createServer(function (conn) {
     conn.on("error", function(){
         console.log('Aucun client connecté');
     });
+    var chunks = [];
     conn.on("data", function(data){
-        data = JSON.parse(data);
+        chunks.push(data);
+        var new_data = JSON.parse(chunks);
         // Comparaison des courriels sur JSON reçu avec ceux du JSON du serveur local
-        for (var i = 0; i < Object.values(data).length; i++) {
-            for (var k = 0; k < Object.values(data)[i]["emails"].length; k++){
-                if (Object.values(my_obj)[i]["emails"].indexOf(Object.values(data)[i]["emails"][k]) < 0){
-                    Object.values(my_obj)[i]["emails"].unshift(Object.values(data)[i]["emails"][k]); 
+        for (var i = 0; i < Object.values(new_data).length; i++) {
+            for (var k = 0; k < Object.values(new_data)[i]["emails"].length; k++){
+                if (Object.values(my_obj)[i]["emails"].indexOf(Object.values(new_data)[i]["emails"][k]) < 0){
+                    Object.values(my_obj)[i]["emails"].unshift(Object.values(new_data)[i]["emails"][k]); 
                 }
             }
         }
-    });
+    });    
 });
 
 var serverListen = 3033;
